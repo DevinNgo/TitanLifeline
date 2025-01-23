bcb21a10899053c00508b0e41ae3a16e9ecf4a5d
devinngo <devinngo03@gmail.com>
2025-01-23 00:14:20 -0800

    aded comments to DateTime

--- src/components/DateTime.jsx
+++ src/components/DateTime.jsx
@@ -5,9 +5,10 @@ export const DateTime = () => {
   var [date, setDate] = useState(new Date());
 
   useEffect(() => {
-
+    // Create a timer that updates the 'date' state every second
     var timer = setInterval(()=>setDate(new Date()), 1000 )
 
+    // Return a cleanup function to clear the timer when the component is unmounted
     return function cleanup() {
       clearInterval(timer)
     }
