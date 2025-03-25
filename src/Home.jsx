39e13ae6d2e6b68cf014bbea44321abf1699af8d
Devinngo <devinngo03@gmail.com>
2025-03-24 17:27:28 -0700

    added files for routing

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,25 +2,15 @@ import './App.css';
 import SvgMap from "./components/SvgMap.jsx";
 import DateTime from "./components/DateTime.jsx";
 import Legend from "./components/Legend.jsx";
-import Home from "./Home";
 import CurrentAvailability from './components/CurrentAvailability.jsx';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+function Home() {
 
   return (
-    <div className="home"> 
-      <div className="header font-bold text-center">
-        <h1 className="text-4xl pt-2">
-          Titan Lifeline
-        </h1>
-        <h1 className="text-2xl pb-2">
-          <DateTime/>
-        </h1>
-        <button className="bg-indigo-300 hover:bg-indigo-400 text-white font-bold py-2 px-4 rounded">
-          Check future parking availability!
-        </button>
-      </div>
+    <div className="home">
+      <button className="bg-indigo-300 hover:bg-indigo-400 text">
+        Check future availability!
+      </button>
       <div className="flex justify-center pt-2">
           <SvgMap/>
           <div className="pl-8 hidden md:block">
@@ -37,4 +27,4 @@ function App() {
   )
 }
 
-export default App
+export default Home
